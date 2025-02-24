646959e3783c7797a7b41141e63b8055a9a8578e
W Rodas <wrodas@choltek.com>
2025-02-23 21:39:15 -0500

    add elements to file

---
+++ script.js
@@ -0,0 +1,27 @@
+// elements
+const screenEl = document.querySelector('.screen');
+const clearEl = document.querySelector('.clear');
+const polarityEl = document.querySelector('.polarity');
+const moduloEl = document.querySelector('.modulo');
+const divideEl = document.querySelector('.divide');
+const multiplyEl = document.querySelector('.multiply');
+const subtractEl = document.querySelector('.subtract');
+const additionEl = document.querySelector('.add');
+const periodEl = document.querySelector('.period');
+const equalsEl = document.querySelector('.equals');
+const zeroEl = document.querySelector('.zero');
+const oneEl = document.querySelector('.one');
+const twoEl = document.querySelector('.two');
+const threeEl = document.querySelector('.three');
+const fourEl = document.querySelector('.four');
+const fiveEl = document.querySelector('.five');
+const sixEl = document.querySelector('.six');
+const sevenEl = document.querySelector('.seven');
+const eightEl = document.querySelector('.eight');
+const nineEl = document.querySelector('.nine');
+
+let digitOne = 0;
+let digitTwo = 0;
+let operator = 0;
+
+screenEl.textContent = '0';
